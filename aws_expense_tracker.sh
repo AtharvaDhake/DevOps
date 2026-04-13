2ac189cd3be3a2b51204b6d9dc87806b88eeb47f
Ubuntu <ubuntu@ip-172-31-18-246.eu-north-1.compute.internal>
2026-04-13 08:56:34 +0000

    First Vesion

---
+++ aws_expense_tracker.sh
@@ -0,0 +1,25 @@
+#1/bin/bash
+
+#########################################
+#Author : Atharva
+#Date ; 12/4/2026
+#
+# Version: v1
+#
+# This script will report the Aws expense
+##########################################
+
+set -x
+
+#list s3 buckets
+aws s3 ls
+
+#list ec2 Instaces
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#list aws lambda functions
+aws lambda list-functions
+
+#list IAM users
+aws iam list-users
+
